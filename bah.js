d1d9b11c9efcc955e8d05aaa49dc1afd66236b19
Vitor <vitor.eduardo2135@gmail.com>
2023-03-15 16:23:36 -0300

    HOME TO APRENDENDO

---
+++ bah.js
@@ -0,0 +1,7 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+console.log(input)
+const inputMinusculo = input.toLowerCase();
+console.log(cidade)
+console.log(inputMinusculo)
+console.log(cidade === inputMinusculo); // true
