c080e1d8072a626c8060f904bdf76596920c9365
Vitor <vitor.eduardo2135@gmail.com>
2023-03-16 08:47:16 -0300

    PQP

--- js.js
+++ js.js
@@ -1,2 +1,2 @@
 console.log(null == undefined); // true
-console.log(null === undefined); // false
+console.log(null === undefined); // false and true cause js is gay
---
+++ kk.js
@@ -0,0 +1,4 @@
+let mirilo = 2
+if (mirilo == 2){
+    console.log("GAY")
+}
