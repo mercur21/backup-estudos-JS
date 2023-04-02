04ca0fe2c7be18d60ce43aa4786aa7e72972fc17
Vitor <vitor.eduardo2135@gmail.com>
2023-04-02 15:39:55 -0300

    New

---
+++ FuncoesArray.js
@@ -0,0 +1,15 @@
+// Arrays podem ser criadas com let, var e const.
+let L = []
+
+L.push(2) // Comando array.push(param) adiciona um elemento à lista.
+
+console.log(L) // A saída será [ 2 ]
+
+/////////////////////////////////////--//////////////////////////////////
+
+let L0 = [1]
+let L1 = [2]
+
+let L2 = L0.concat(L1) // Função array.concat(param) concatena/soma duas arrays.
+
+console.log(L2) // Saída [ 1, 2 ]
---
+++ t.js
@@ -0,0 +1,2 @@
+let conta = 4.555 + 8.999
+console.log(conta) 
