58ea3d4289576ce8a16d86d07f87f19aa04d61b9
Vitor <vitor.eduardo2135@gmail.com>
2023-04-07 13:06:41 -0300

    Backup

--- FuncoesArray.js
+++ FuncoesArray.js
@@ -45,3 +45,37 @@ let lista5 = [1,2,3,4,5]
 lista5.shift() // Função array.shift() retira o primeiro elemento da lista.
 
 console.log(lista5) // Saída [ 2, 3, 4, 5 ]
+
+//////////////////////////////////--/////////////////////////
+
+// Função slice não altera a lista/valor inicial 
+let lista6 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+let lista7 = lista6.slice(0, lista6.length / 2) // A função array.slice(param1, param2) fatia/divide uma array do param1 até o param2.
+let lista8 = lista6.slice(lista6.length / 2) // A função array.slice(param2) já entende que é do parametro citado até o fim da lista.
+
+console.log(lista7) // Saída [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+console.log(lista8) // Saída [ 11, 12, 13, 14, 15,16, 17, 18, 19, 20 ]
+
+//////////////////////////////////--///////////////////////////
+
+let lista9 = [1,2,3,4,5,6,7]
+
+lista9.splice(1,2, "Olokinho meu") // Função array.splice(inicio, fim, elemento-opcional) (fim é opcional pois ele pode remover todos os elementos em sua falta) ela remove elementos com índices 
+//específicos. Splice também pode adicionar elementos no lugar de outros removidos.
+
+console.log(lista9) // SAída [ 1, 4, 5, 6, 7 ]
+
+/////////////////////////--//////////////////////////
+
+let lista10 = [1,2,3,4,5,6]
+let lista11 = [lista10]
+
+if (lista11[0].includes(1)){ // Função array.includes(param) verifica se o param está incluído retornando true.
+    console.log("Artur coringando")
+}
+if (lista10.includes(1)){ // Exemplo com lista de duas dimensões
+    console.log("Artur coringando")
+}
+
+/////////////////////////////--////////////////////////////////////
